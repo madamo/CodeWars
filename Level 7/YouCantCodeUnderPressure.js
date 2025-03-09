9e60862d858126b7e0344d2d64cedec10182d6a3
madamo <mattadamo@gmail.com>
2025-03-09 10:44:23 -0700

    March 9 exercises

---
+++ Level 7/YouCantCodeUnderPressure.js
@@ -0,0 +1,13 @@
+/*
+
+    Description:
+    Code as fast as you can! You need to double the integer and return it.
+*/
+
+/* Solution */
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i = i * 2
+    return i;
+}
