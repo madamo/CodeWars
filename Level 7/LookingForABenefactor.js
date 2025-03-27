d8d323a5b6aedf12534d8096de40a55c54c84e97
madamo <mattadamo@gmail.com>
2025-03-27 13:19:32 -0700

    March 27 exercises

---
+++ Level 7/LookingForABenefactor.js
@@ -0,0 +1,42 @@
+/*
+    Description:
+
+    The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he could have made a mistake.
+
+    Could you help him?
+
+    Task
+
+    The function should return the expected donation (rounded up to the next integer) that will permit to reach the average.
+
+    Should the last donation be a non positive number (<= 0) John wants us either to throw an error or return such value so he will clearly see that his expectations are not great enough.
+
+    In "Sample Tests" you can see what to return.
+
+    Notes:
+
+    all donations and new average are numbers (integers or floats), array of donations can be empty.
+    See examples below and "Sample Tests" to see which return is to be done.
+    array: [14, 30, 5, 7, 9, 11, 15], new average: 92 -> result: 645
+    array: [14, 30, 5, 7, 9, 11, 15], new average: 2  -> result: variable, see "Sample Tests" 
+
+*/
+
+/* Solution */
+
+function newAvg(arr, newavg) {
+    if (arr.length <= 0) {
+      return newavg
+    }
+    
+    let sum = arr.reduce((acc, curr) => acc + curr)
+    let length = arr.length + 1
+    let donation = Math.ceil((newavg * length) - sum)
+    
+    if (donation < 0) {
+      throw new Error("Expected New Average is too low")
+    } else {
+        return donation
+    }
+  
+  }
