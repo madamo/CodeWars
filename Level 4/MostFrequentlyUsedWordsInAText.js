19a242c8ddaf77af884e095dd2f73c79cbf4ab1f
madamo <mattadamo@gmail.com>
2025-03-28 10:14:29 -0700

    March 28 exercises

--- Level 4/MostFrequentlyUsedWordsInAText.js
+++ Level 4/MostFrequentlyUsedWordsInAText.js
@@ -12,3 +12,36 @@ Matches should be case-insensitive, and the words in the result should be lowerc
 Ties may be broken arbitrarily.
 If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
 */
+
+/* Solution */
+
+function topThreeWords(text) {
+    // Split the text, remove whitespace and extraneous characters
+    let wordArr = text.toLowerCase().split(" ")
+    const re = /\w+/
+    const remove = /\/\//g
+    let filteredList = wordArr.filter((word) => re.test(word))
+    filteredList = filteredList.map((word) => word.replace(remove, ""))
+  
+    // Count number of occurences of each word
+    let wordCount = {}
+      
+    for (let word of filteredList) { 
+      wordCount[word] = (wordCount[word] || 0) + 1
+    }
+    
+    // Find the top 3 (or less) words
+    
+    let freqWords = []
+    const topValues = Object.values(wordCount).sort((a,b) => b - a )
+    
+    for (let i = 0; i < 3; i++ ) {
+      let topWord = Object.keys(wordCount).find((key) => wordCount[key] === topValues[i])
+      if (topWord) {
+        freqWords.push(topWord)
+      }
+    }
+    
+    return freqWords
+    
+  }
