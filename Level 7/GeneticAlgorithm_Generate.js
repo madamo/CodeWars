cedad43a1d5d7ed79dfa0a198fc9e9da42b13b1a
madamo <mattadamo@gmail.com>
2025-04-03 09:00:59 -0700

    April 3 exercises

---
+++ Level 7/GeneticAlgorithm_Generate.js
@@ -0,0 +1,23 @@
+/*
+    Description:
+
+    A genetic algorithm is based in groups of chromosomes, called populations. To start our population of chromosomes we need to generate random binary strings with a specified length.
+
+    In this kata you have to implement a function generate that receives a length and has to return a random binary strign with length characters.
+
+
+
+    Example:
+
+    Generate a chromosome with length of 4 generate(4) could return the chromosome 0010, 1110, 1111... or any of 2^4 possibilities.
+*/
+
+/* Solution */
+
+const generate = length => {
+    let binaryStr = ""
+    for (let i = 0; i < length; i++) {
+      binaryStr += Math.floor(Math.random() * (Math.floor(1) - Math.ceil(0) + 1) + Math.ceil(0));
+    }
+    return binaryStr
+};
