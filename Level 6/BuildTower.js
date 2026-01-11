7a26c434d8839347140e2f18a7e31a82c048da28
madamo <mattadamo@gmail.com>
2026-01-10 16:38:59 -0800

    Jan 10 exercises

--- Level 6/BuildTower.js
+++ Level 6/BuildTower.js
@@ -4,22 +4,31 @@
 function towerBuilder(nFloors) {
   let tower = []
   let floor = ""
+  let left = 0
+  let right = 0
+  let pad = 0
+  console.log('nFloors:', nFloors)
   for (let i = nFloors; i > 0; i--) {
     console.log("i:", i)
     for (let j = 0; j < (nFloors * 2) - 1; j++) {
       console.log("j", j)
-      if (i < nFloors && j !== 2) {
-        floor += " "
-      } else {
+      console.log("pad:", pad)
+      if (i === nFloors) {
         floor += "*"
-
+      } else if (j < pad || j >= (nFloors * 2) - pad*2) {
+        floor += " "
       }
+      else {
+        floor += "*"
+      }
+
       console.log("floor", floor)
 
     }
     
     tower.unshift(floor)
     floor = ""
+    pad++
   }
   
   console.log("tower", tower)
