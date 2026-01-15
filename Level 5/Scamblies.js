cb76f5552cd074b7dafbc8bbdee6991b183d8d09
madamo <mattadamo@gmail.com>
2026-01-14 21:23:44 -0800

    Jan 14 exercises

--- Level 5/Scamblies.js
+++ Level 5/Scamblies.js
@@ -3,15 +3,19 @@
 /* Solution */
 
 function scramble(str1, str2) {
-  console.log("str1", str1)
-  console.log("str2", str2)
+  //console.log("str1", str1)
+ // console.log("str2", str2)
   let contains = false
   for (let i = 0; i < str2.length; i++) {
-    if (str1.includes(str2[i])) {
+    //console.log(str2[i])
+    let re = new RegExp(String.raw`${str2[i]}`, "g") 
+    //console.log(re)
+    if ( (str1.match(re) || []).length >= (str2.match(re) || []).length ) {
+      //console.log(`str1: ${(str1.match(re) || []).length} str2: ${(str2.match(re) || []).length}`)
       contains = true
     } else {
       return false
-    }
+    } 
   }
   return contains;
 }
