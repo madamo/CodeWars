da9682604e08299730ef7b4ca676ee8f11bc4240
madamo <mattadamo@gmail.com>
2026-01-17 21:55:11 -0800

    Jan 17 exercises

---
+++ Level 5/PaginationHelper.js
@@ -0,0 +1,24 @@
+/* Description */
+
+/* Solution */
+
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	// The constructor takes in an array of items and a integer indicating how many
+	// items fit within a single page
+	}
+	itemCount() {
+	// returns the number of items within the entire collection
+	}
+	pageCount() {
+	// returns the number of pages
+	}
+	pageItemCount(pageIndex) {
+	// returns the number of items on the current page. page_index is zero based.
+	// this method should return -1 for pageIndex values that are out of range
+	}
+	pageIndex(itemIndex) {
+	// determines what page an item is on. Zero based indexes
+	// this method should return -1 for itemIndex values that are out of range
+	}
+}
