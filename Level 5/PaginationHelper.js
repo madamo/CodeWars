72e86876f0788426d5986ffc5fcfc823148f8a7f
madamo <mattadamo@gmail.com>
2026-01-20 22:15:01 -0800

    Jan 20 exercises

--- Level 5/PaginationHelper.js
+++ Level 5/PaginationHelper.js
@@ -21,13 +21,17 @@ class PaginationHelper {
 	pageItemCount(pageIndex) {
 	// returns the number of items on the current page. page_index is zero based.
 	// this method should return -1 for pageIndex values that are out of range
-    if (pageIndex > this.pageCount() || pageIndex < 0) {
+    if (pageIndex >= this.pageCount() || pageIndex < 0) {
       console.log('number of pages', this.pageCount())
       console.log('index out of range')
       return -1
     } else if (pageIndex === this.pageCount()-1) {
       console.log('last page')
-      return this.itemCount() % this.itemsPerPage
+      if (this.itemCount() % this.itesmPerPage === 0) {
+        return this.itemsPerPage()
+      } else {
+        return this.itemCount() % this.itemsPerPage
+      }
     } else {
       return this.itemsPerPage
     }
@@ -35,5 +39,14 @@ class PaginationHelper {
 	pageIndex(itemIndex) {
 	// determines what page an item is on. Zero based indexes
 	// this method should return -1 for itemIndex values that are out of range
+    if (itemIndex > this.collection.length || itemIndex < 0) {
+      return -1
+    } else if (itemIndex === 0 && this.collection.length === 0) {
+      return -1
+    } else if (itemIndex === 0) {
+      return 0
+    } else {
+      return Math.round(itemIndex / this.itemsPerPage)
+    }
 	}
 }
