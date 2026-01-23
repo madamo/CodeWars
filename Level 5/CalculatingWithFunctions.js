2e791866b1821bddb9fd8aadf4b30937af694a71
madamo <mattadamo@gmail.com>
2026-01-22 22:42:36 -0800

    Jan 22 exercises

---
+++ Level 5/CalculatingWithFunctions.js
@@ -0,0 +1,43 @@
+/* Description */
+
+/* Solution */
+
+function zero() {
+  return 0
+}
+function one() {
+  return 1
+}
+function two() {
+  return 2
+}
+function three() {
+  return 3
+}
+function four() {
+  return 4
+}
+function five() {
+  return 5
+}
+function six() {
+  return 6
+}
+function seven(operation) {
+  return 7
+}
+function eight() {
+  return 8
+}
+function nine() {
+  return 9
+}
+
+function plus(x, y) {
+  return x + y
+}
+function minus() {}
+function times(x, y) {
+  return x * y
+}
+function dividedBy() {}
