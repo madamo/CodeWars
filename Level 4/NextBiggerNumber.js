a77b62fa885ac7b74b3a0a3eb196a60c12969d2e
madamo <mattadamo@gmail.com>
2026-02-02 22:23:07 -0800

    Feb 2 exercises

--- Level 4/NextBiggerNumber.js
+++ Level 4/NextBiggerNumber.js
@@ -10,14 +10,19 @@ function nextBigger(n){
   const numString = n.toString()
   const digitArr = numString.slice(-(numString.length)).split("")
   let biggerStr = ""
+  let newArr = digitArr.slice()
   
   for (let i = 0; i < digitArr.length; i++) {
+    console.log(digitArr[i])
     if (digitArr[i] < digitArr[i+1]) {
       console.log('bigger')
-      biggerStr += digitArr[i+1] + digitArr[i]
+      newArr.splice(i, 2, digitArr[i+1], digitArr[i])
+      
+      //biggerStr += digitArr[i+1] + digitArr[i]
     }
   }
 
+  console.log(newArr)
   
-  return parseInt(biggerStr)
+  return parseInt(newArr.join(""))
 }
